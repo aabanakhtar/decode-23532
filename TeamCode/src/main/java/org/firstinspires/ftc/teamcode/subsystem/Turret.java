2f58d453a611a7693dc936cd454e6b0b83476ae2
aaban akhtar <aabanakhtar.home@gmail.com>
2025-10-21 09:41:31 -0400

    add aimbot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Turret.java
@@ -20,6 +20,7 @@ public class Turret extends SubsystemBase {
 
     public static boolean tuning = false;
     public static double targetPower = 0.0;
+    public static double targetAngle = 0.0;
     public static double kP = 0.0;
     public static double kI = 0.0;
     public static double kD = 0.0;
@@ -27,8 +28,8 @@ public class Turret extends SubsystemBase {
     // 312 RPM Yellow Jacket with gearing 27t to 95t
     public static final double GEAR_RATIO = 95.0 / 27.0; // motor rotations per turret rotation
     public static final double TURRET_ENCODER_CPR = 537.7 * GEAR_RATIO; // ≈ 1891.6 ticks per turret rotation
-    public static final double TURRET_MAX_ANGLE = 84.5;
-    public static final double TURRET_HOME_OFFSET = TURRET_ENCODER_CPR * (TURRET_MAX_ANGLE / 360.0);
+    public static final double TURRET_MAX_ANGLE = 84.56;
+    public static final double TURRET_HOME_OFFSET = TURRET_ENCODER_CPR * (TURRET_MAX_ANGLE / 360.0); // to set right to negative
 
     public static double homingPower = 0.0;
     public static PIDFController controller = new PIDFController(kP, kI, kD, 0);
@@ -42,13 +43,18 @@ public class Turret extends SubsystemBase {
         robot.telemetry.addLine("==========TURRET===========");
         robot.telemetry.addData("Encoder position", robot.shooterTurret.encoder.getPosition());
         robot.telemetry.addData("angle", calculateAngleFromEncoder());
+        robot.telemetry.addData("target angle", targetAngle);
+        robot.telemetry.addData("target power:", targetPower);
 
         switch (mode) {
             case RAW:
                 robot.shooterTurret.set(targetPower);
                 break;
             case AIMING:
-
+                // aim within hardware limits
+                double targetAngleFixed = Math.max(-TURRET_MAX_ANGLE, Math.min(TURRET_MAX_ANGLE, targetAngle));
+                double power = controller.calculate(calculateAngleFromEncoder(), targetAngleFixed);
+                robot.shooterTurret.set(power);
                 break;
             case HOMING:
                 robot.shooterTurret.set(homingPower);
@@ -70,7 +76,7 @@ public class Turret extends SubsystemBase {
 
     private double calculateAngleFromEncoder() {
         // right = negative in standard mathematics ( i think )
-        double encoderPos = TURRET_HOME_OFFSET - robot.shooterTurret.encoder.getPosition();
+        double encoderPos = TURRET_HOME_OFFSET + robot.shooterTurret.encoder.getPosition();
         double angle = (encoderPos / TURRET_ENCODER_CPR) * 360.0;
         angle = Math.max(-TURRET_MAX_ANGLE, Math.min(TURRET_MAX_ANGLE, angle));
         return angle;
